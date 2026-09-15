f2a08c10727c889730ccd3f99b800256e25ca057
Felipe Arroyo <felipevieira.arroyo@gmail.com>
2026-09-15 16:02:02 -0300

    feat: CRUD completo de pacientes y medicos con decoradores de NestJS

--- src/medicos/medicos.service.ts
+++ src/medicos/medicos.service.ts
@@ -3,9 +3,34 @@ import { PrismaService } from '../prisma/prisma.service.js';
 
 @Injectable()
 export class MedicosService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
   findAll() {
     return this.prisma.medico.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.medico.findUnique({
+      where: { id },
+    });
+  }
+
+  create(data: any) {
+    return this.prisma.medico.create({
+      data,
+    });
+  }
+
+  update(id: number, data: any) {
+    return this.prisma.medico.update({
+      where: { id },
+      data,
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.medico.delete({
+      where: { id },
+    });
   }
 }
